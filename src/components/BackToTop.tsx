7274859efefa4f2afa582713053e3dbbdf8bc2e4
Tien Dat Pham <dat20036@gmail.com>
2025-10-06 12:06:26 +0700

    add back-to-top button.

---
+++ src/components/BackToTop.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { clsx } from "clsx";
+import { ArrowUpToLine } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function BackToTop() {
+    const [showTopBtn, setShowTopBtn] = useState(false);
+
+    const goToTop = useCallback(() => {
+        window.scroll({
+            top: 0,
+            left: 0,
+        });
+    }, []);
+
+    const scrollHandle = useCallback(() => {
+        if (window.scrollY >= 25) {
+            setShowTopBtn(true);
+        } else {
+            setShowTopBtn(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandle);
+
+        return () => window.removeEventListener("scroll", scrollHandle);
+    }, [scrollHandle]);
+
+    return (
+        <Button
+            className={clsx("bg-primary bottom-4 right-4", showTopBtn && "fixed", !showTopBtn && "hidden")}
+            size="icon-lg"
+            onClick={goToTop}
+            aria-label="back-to-top">
+            <ArrowUpToLine />
+        </Button>
+    );
+}
